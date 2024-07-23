ce059923757139cc4b49c26adc4f5b0d4d8678c2
redouane <redouane.hitachi@gmail.com>
2024-07-22 17:36:15 -0700

    Handling Input Elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+
+import SearchBar from './components/SearchBar'
 function App() {
+
+  const hundleSubmit = (term) => {
+    console.log('do a search with terme ', term);
+  };
   return (
     <div>
-
+      <SearchBar onSubmit={hundleSubmit} />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function searchBar() {
+export default function SearchBar({ onSubmit }) {
+  const [terme, setTerme] = useState('');
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(terme);
+  };
+  const handleChange = (event) => {
+    setTerme(event.target.value);
+  };
+
   return (
     <div>
-      searchBar
+      <form onSubmit={handleFormSubmit} >
+        <input type="text"
+          value={terme}
+          onChange={handleChange} />
+      </form>
     </div>
+
   )
 }
